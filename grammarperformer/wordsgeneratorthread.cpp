de0ad6884542199b3f92e89a3ffb446f0ca86033
malashkin.andrey@mail.ru <malashkin.andrey@mail.ru>
2014-04-23 22:52:13 +0400

    forgiven files

---
+++ grammarperformer/wordsgeneratorthread.cpp
@@ -0,0 +1,37 @@
+#include <QDebug>
+#include <QMutexLocker>
+
+#include "wordsgeneratorthread.h"
+#include "wordsgenerator.h"
+
+WordsGeneratorThread::WordsGeneratorThread()
+    : m_generator(NULL)
+{}
+
+WordsGeneratorThread::WordsGeneratorThread(WordsGenerator* _generator)
+    : m_generator(_generator)
+{}
+
+WordsGeneratorThread::~WordsGeneratorThread()
+{
+    delete m_generator;
+}
+
+void WordsGeneratorThread::setGenerator(WordsGenerator* _generator)
+{
+    delete m_generator;
+    m_generator = _generator;
+}
+
+void WordsGeneratorThread::generateTillLevel(int _level)
+{
+    qDebug() << isRunning();
+    m_generator->moveToThread(this);
+    start();
+    m_generator->begin(_level);
+}
+
+const WordsGenerator * const WordsGeneratorThread::generator() const
+{
+    return m_generator;
+}
---
+++ grammarperformer/wordsgeneratorthread.h
@@ -0,0 +1,26 @@
+#ifndef WORDSGENERATORTHREAD_H
+#define WORDSGENERATORTHREAD_H
+
+#include <QThread>
+#include <QMutexLocker>
+
+class WordsGenerator;
+
+class WordsGeneratorThread : public QThread
+{
+public:
+    WordsGeneratorThread();
+    WordsGeneratorThread(WordsGenerator* _generator);
+    ~WordsGeneratorThread();
+
+    void setGenerator(WordsGenerator* _generator);
+    void generateTillLevel(int _level);
+
+    const WordsGenerator* const generator() const;
+
+private:
+    QMutex m_mutex;
+    WordsGenerator* m_generator;
+};
+
+#endif // WORDSGENERATORTHREAD_H
