cdfffe787f6aa518e54abd2184b932b4de661e78
RuZK <arruzk@gmail.com>
2014-04-28 12:17:55 +0400

    change charset

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -38,7 +38,7 @@ void MainWindow::loadPainter()
 {
     for(int i = 1; i<=3;i++){
         BasePainter *p = new FlowPainter;
-        p->loadBlocks(QString(":data/style")+QString::number(i)+QString(".json"), QString("Ñòèëü ")+QString::number(i));
+        p->loadBlocks(QString(":data/style")+QString::number(i)+QString(".json"), QString("Ð¡Ñ‚Ð¸Ð»ÑŒ ")+QString::number(i));
         painters.append(p);
     }
 }
